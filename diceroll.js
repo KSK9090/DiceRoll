76b578ee8c7fae8a03f18f6da63d16cdd17d7de5
KSK9090 <kemal.kaya@ymail.com>
2026-03-06 08:55:12 +0100

    erster commit

---
+++ diceroll.js
@@ -0,0 +1,47 @@
+let gewählterWürfel;
+document.getElementById("würfel4").onclick = function(){
+    gewählterWürfel = 4;
+}
+
+document.getElementById("würfel6").onclick = function(){
+    gewählterWürfel = 6;
+}
+
+document.getElementById("würfel8").onclick = function(){
+    gewählterWürfel = 8;
+}
+
+document.getElementById("würfel10").onclick = function(){
+    gewählterWürfel = 10;
+}
+
+document.getElementById("würfel12").onclick = function(){
+    gewählterWürfel = 12;
+}
+
+document.getElementById("würfel20").onclick = function(){
+    gewählterWürfel = 20;
+}
+document.getElementById("würfelButton").onclick = function(){
+    
+    let inputAnzahl = Number(document.getElementById("inputAnzahl").value);
+    let start = 0;
+    label1.textContent = "";
+
+    let gesamt = 0;
+
+    while(start < inputAnzahl){
+
+        let wurf = Math.floor(Math.random() * gewählterWürfel) + 1;
+        label1.textContent += wurf + " ";
+        gesamt += wurf;
+        start++;
+    }
+
+    if(inputAnzahl > 1){
+        label1.innerHTML += "\n--------\n" + gesamt;
+    }
+
+
+
+}
